2f9e378e8e1775be9d745f306de7f4a722cd3ef9
Brad Hyslop @ mimic <bhyslop@scaleinvariant.org>
2023-09-16 17:58:24 -0700

    With Paul, alter the means of discerning episode numbers for known podcasts.

--- app/process.py
+++ app/process.py
@@ -20,7 +20,6 @@ class Episode:
     directory: str = None
     pub_date: str = None
     site_directory: str = None
-    links: list[EpisodeLink] = None
 
 
 OctoAI = {
@@ -37,15 +36,11 @@ class Podcast:
     name: str  # Unix style, short lowercase, used as a parent directory
     last_notified: int  # TODO
     rss_url: str
+    number_extractor_function: object
 
 
 logging.basicConfig(level=logging.INFO, format='%(pathname)s(%(lineno)s): %(levelname)s %(message)s')
 log = logging.getLogger()
-
-tgn = Podcast('tgn', 254, 'https://feeds.buzzsprout.com/2049759.rss')
-wcl = Podcast('wcl', 254, 'https://feed.podbean.com/the40and20podcast/feed.xml')
-# Iterable to loop over
-podcasts = [tgn, wcl]
 
 
 # Regex to pull the first number from a title - podcast number, in this case. Heuristic but works almost every time.
@@ -58,28 +53,74 @@ url_rs = r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\
 url_matcher = re.compile(url_rs)
 
 
-def episode_number(entry, index_notfound) -> tuple:
-    # We try four ways to get the episode number, since it's our primary key for everything.
-    # Metadata is best, title string second, special-case list third, and lastly we create a list.
-    # Tuple returned - gotta increment the last-unknown number if we used it.
-    if 'itunes:episode' in entry:
-        number = entry['itunes:episode']
-        return number, index_notfound
+wcl_lookup = {
+    "Watch Clicker Mini Review - Nodus Sector Dive": 81.5,
+    "Episode 36 GMT Watches": 36,
+    }
 
-    log.debug(f'Unable to parse episode ID from title {entry["title"]}, trying TGN special list')
-    rc = match_missing_numbers(entry['title'])
-    if rc:
-        log.info(f"Found in TGN exception list {rc=}")
-        return rc, index_notfound
 
-    log.debug(f'No exception number found for {entry["title"]}, trying regex...')
-    groups = title_matcher.search(entry['title'])
-    if groups:
-        log.debug(f'Found episode ID {groups[0]} from title')
-        return int(groups[0]), index_notfound
+def episode_number_wcl(entry):
 
-    log.warning(f'Unable to determine number, using notfound index {index_notfound} for {entry["title"]}')
-    return index_notfound, index_notfound + 1
+    meta_entry = entry.get('itunes:episode', None)
+    if meta_entry is not None: return float(meta_entry)
+
+    title = entry['title']
+    hardcode = wcl_lookup.get(title, None)
+    if hardcode is not None: return float(hardcode)
+
+    asSplit = re.split(r'[-‒–—\:]', title)
+
+    deprefixed = asSplit[0].lower().removeprefix('episode').strip()
+    if deprefixed.isdigit():
+        return float(deprefixed)
+
+    log.warning(f"FAIL: -> {title}")
+    return None
+
+
+
+tgn_lookup = {
+    "Drafting High-End Watches With A Sense Of Adventure – A TGN Special With Collective Horology": 214.5,
+    "The Grey NATO – 206 Re-Reupload – New Watches! Pelagos 39, Diver's Sixty-Five 12H, And The Steel Doxa Army": 206.5,
+    "The Grey NATO – A Week Off (And A Request!)": 160.5,
+    "Depth Charge - The Original Soundtrack by Oran Chan": 143.5,
+    "The Grey Nato Ep 25  - Dream Watches 2017": 25,
+    "The Grey Nato - Question & Answer #1": 20.5,
+    "TGN Chats - Merlin Schwertner (Nomos Watches) And Jason Gallop (Roldorf & Co)": 16.5,
+    "TGN Chats - Chase Fancher :: Oak & Oscar": 14.5,
+    } 
+
+
+def episode_number_tgn(entry):
+
+    title = entry['title']
+
+    hardcode = tgn_lookup.get(title, None)
+    if hardcode is not None:
+        return float(hardcode)
+
+    asSplit = re.split(r'[-‒–—]', title)
+    if len(asSplit) < 2:
+        log.warning(f"FAIL: -> {title}")
+        return None
+
+    second = asSplit[1].strip()
+    if second.isdigit():
+        return float(second)
+    elif second.lower().startswith('ep'):
+        subsplit = second.split()
+        if len(subsplit) == 2 and subsplit[1].isdigit():
+            return float(subsplit[1])
+
+    log.warning(f"FAIL: -> {title}")
+    return None
+
+
+# Iterable to loop over
+podcasts = [
+      Podcast('tgn', 254, 'https://feeds.buzzsprout.com/2049759.rss', episode_number_tgn),
+      Podcast('wcl', 254, 'https://feed.podbean.com/the40and20podcast/feed.xml', episode_number_wcl),
+      ]
 
 
 def episode_url(entry, default_url='https://thegreynato.com/'):
@@ -131,9 +172,6 @@ def unwrap_bitly(url: str) -> str:
 def process_all_podcasts():
     for podcast in podcasts:
         count = 0
-        index_notfound = 2100
-        # Keep a Set of episode numbers to bug-check the unnumbered episode logic
-        seen_episodes = set()
 
         log.info(f'Processing {podcast}')
         basedir = Path('podcasts', podcast.name)
@@ -153,16 +191,17 @@ def process_all_podcasts():
         mkdocs_mainpage.write_text(f"### Page updated {datetime.now().astimezone().isoformat()} - {ep_count} episodes\n")
         log.info(f"Found {ep_count} episodes in {podcast.name}")
 
+        fail_count = 0
+
         # This loop is over all episodes in the current podcast
         for entry in entries['rss']['channel']['item']:
+
+            be_number = podcast.number_extractor_function(entry)
+            if be_number is None:
+                fail_count += 1 # TODO
+
             episode = Episode()
-            episode.number, index_notfound = episode_number(entry, index_notfound)
-            if episode.number in seen_episodes:
-                # TGN 206 was re-uploaded. FML.
-                log.warning(f"Ignoring duplicate {episode.number} in {podcast.name}")
-                # log.error(f"FATAL: Duplicate episode number {episode.number} in {podcast}, stopping")
-                continue
-            seen_episodes.add(episode.number)
+            episode.number = be_number
             episode.episode_url = unwrap_bitly(episode_url(entry))
             if 'subtitle' in entry:
                 episode.subtitle = entry['subtitle']
@@ -182,7 +221,7 @@ def process_all_podcasts():
             # Rewrite as POSIX path, as basic paths can't serialize to JSON
             episode.directory = episode.directory.as_posix()
 
-            # mkdocs director¥ for this episode - sites/tgn/docs/40 for example
+            # mkdocs directory for this episode - sites/tgn/docs/40 for example
             episode.site_directory = Path('sites', podcast.name, 'docs', str(episode.number)).absolute()
             if not episode.site_directory.exists():
                 log.debug(f"Creating site directory {episode.site_directory}")
@@ -199,6 +238,10 @@ def process_all_podcasts():
                 ep_index.write(f"- [{episode.title}]({str(episode.number)}/episode.md) {episode.pub_date}\n")
             count += 1
         # Done with this podcast - check episode count
+
+        if fail_count:
+            log.warning(f"UNDISCERNABLE EPISODES: -> {fail_count=}")
+
         if count == ep_count:
             log.info(f"Processed all {ep_count} episodes in {podcast.name}")
         else:
@@ -207,3 +250,4 @@ def process_all_podcasts():
 
 if __name__ == '__main__':
     process_all_podcasts()
+
