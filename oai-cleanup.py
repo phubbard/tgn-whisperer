1f6ff5cdec72b084fe8266436bd87440e9e06962
Paul Hubbard <phubbard@gmail.com>
2023-09-30 11:06:24 -0700

    most basic attribution works as does console UI for same

--- oai-cleanup.py
+++ oai-cleanup.py
@@ -1,19 +1,56 @@
 #!/usr/bin/env python3
 
 # OctoAI returns an annoying format. This script chunks that up. Seems like they should provide this, but whatevs.
-from collections import defaultdict, Counter
+# Now that chunking works, I want a fast, interactive way to do speaker attribution, such that the transcript has
+# proper names on it. I had code that did one TGN intro, but i wasn't happy with it so I'm trying a new approach.
+
+from collections import defaultdict
 import json
-import logging
-from pprint import pprint
 
-logging.basicConfig(level=logging.INFO, format='%(pathname)s(%(lineno)s): %(levelname)s %(message)s')
-log = logging.getLogger()
+# See https://github.com/petereon/beaupy
+from beaupy import confirm, prompt, select, select_multiple, Config
+from rich.console import Console
+config = Config()
+config.raise_on_interrupt = True
+console = Console()
+
+# Heuristics - look for 'Interview' in description
+# Look for Andrew or James in first few utterances and prompt for 'is this correct' => 80% solution
+# Consider moving speaker map to a separate json file / make target as a next step. then can run
+# 'make attribution' as an interactive task but main task runs unattended.
 
 
-if __name__ == '__main__':
-    log.info('Reading episode...')
+def attribute_podcast(speakers: set, lines: list, possible_speakers: set) -> dict:
+    # Build a lookup table / map e.g.
+    # SPEAKER_00 : "James Stacey"
+    speaker_map = defaultdict(lambda: 'Unknown')
+    unknown_speakers = possible_speakers
+    done = False
+    while unknown_speakers and not done:
+        rc = lines.copy()
+        unkn_lines = [x for x in rc if x[1] not in speaker_map]
+        if not unkn_lines:
+            done = True
+            continue
+        line = unkn_lines[0]
+        console.print(f"Which speaker said '{line[2]}'?")
+        cur_speaker = select(list(unknown_speakers))
+        if cur_speaker:
+            speaker_map[line[1]] = cur_speaker
+            unknown_speakers.difference(cur_speaker)
+        else:
+            done = True
+
+    console.print(speaker_map)
+    if confirm("Is that map correct and complete?", default_is_yes=True):
+        return speaker_map
+    return attribute_podcast(speakers, lines, possible_speakers)
+
+
+def process_transcription():
+    console.print('Reading episode...')
     episode = json.load(open('episode-transcribed.json', 'r'))
-    log.info('Processing')
+    console.print('Processing')
     # The default format has chunks of text. We want to append them until the speaker changes.
     rc = []
     speaker = None
@@ -24,7 +61,7 @@ if __name__ == '__main__':
         if speaker != chunk['speaker']:
             # Dump the buffered output
             if speaker:
-                print(f"{start} {speaker} {text_chunk}")
+                # print(f"{start} {speaker} {text_chunk}")
                 rc.append((start, speaker, text_chunk))
 
             speaker = chunk['speaker']
@@ -33,55 +70,27 @@ if __name__ == '__main__':
         else:
             text_chunk += chunk['text']
 
-    print(f"{start} {speaker} {text_chunk}")
+    # print(f"{start} {speaker} {text_chunk}")
     rc.append((start, speaker, text_chunk))
 
     # Now we have an array of chunks, each of which is a tuple of start time, speaker, text. Can we name the speakers?
-    log.info('Attributing...')
+    console.print('Attributing...')
+
     speakers = set()
     # Count how many speakers were found
     for chunk in rc:
         speakers.add(chunk[1])
-    log.info(f"{len(speakers)=} found")
+    console.print(f"{len(speakers)} unique speakers found")
 
-    # Build a lookup table / map e.g.
-    # SPEAKER_00 : "James Stacey"
-    speaker_map = defaultdict(lambda: 'Unknown')
+    for_attrib = []
+    for speaker in speakers:
+        for line in rc:
+            if speaker == line[1]:
+                for_attrib.append(line)
+                break
 
-    s_counter = Counter()
-
-    usual_suspects = {
-        'james': 'James Stacey',
-        'jason': 'Jason Heaton',
-        'andrew': 'Andrew',
-        'everett': 'Everett'
-    }
-
-    # Look in the first few chunks for speakers identifying themselves
-    for idx in range(10):
-        if rc[idx][2].lower().find('james') >= 0:
-            if s_counter['james'] > 0:
-                continue
-            speaker_map[rc[idx][1]] = 'James Stacey'
-
-            s_counter.update(['james'])
-        if rc[idx][2].lower().find('jason') >= 0:
-            if s_counter['jason'] > 0:
-                continue
-            speaker_map[rc[idx][1]] = 'Jason Heaton'
-            s_counter.update(['jason'])
-
-        if rc[idx][2].lower().find('andrew') >= 0:
-            if s_counter['andrew'] > 0:
-                continue
-            speaker_map[rc[idx][1]] = 'Andrew'
-            s_counter.update(['andrew'])
-        if rc[idx][2].lower().find('everett') >= 0:
-            if s_counter['jason'] > 0:
-                continue
-            speaker_map[rc[idx][1]] = 'Jason Heaton'
-            s_counter.update(['jason'])
-
+    # TODO Filter - send one line from each speaker, not just first few lines.
+    speaker_map = attribute_podcast(speakers, for_attrib, set(['James Stacey', 'Jason Heaton', 'Unknown']))
     print(f'{speaker_map=}')
 
     for idx, _ in enumerate(rc):
@@ -95,3 +104,7 @@ if __name__ == '__main__':
     fh = open('episode.md', 'w')
     fh.write(body)
     fh.close()
+
+
+if __name__ == '__main__':
+    process_transcription()
